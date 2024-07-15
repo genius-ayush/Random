a859b7737df2a8f70b38c4ce40e5a8d799e3798b
genius-ayush <ayushsanjayrawal@gmail.com>
2024-07-15 22:29:21 +0530

    BMI calculator

--- SmallProjects/ColorSchemeSwitcher/script.js
+++ SmallProjects/ColorSchemeSwitcher/script.js
@@ -6,8 +6,6 @@ var body = document.querySelector("body")
 colors.forEach(function(color){
     console.log(color) ; 
     color.addEventListener('click' , function(e){
-        console.log(e) ; 
-        console.log(e.target) ; 
         body.style.backgroundColor = e.target.className
     })
 })

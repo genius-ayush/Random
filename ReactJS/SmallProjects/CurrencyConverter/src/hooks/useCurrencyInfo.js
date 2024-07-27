9136f1034fca0d4a259a9358f3701cd25d7fa319
genius-ayush <ayushsanjayrawal@gmail.com>
2024-07-27 21:46:39 +0530

    docker

--- ReactJS/SmallProjects/CurrencyConverter/src/hooks/useCurrencyInfo.js
+++ ReactJS/SmallProjects/CurrencyConverter/src/hooks/useCurrencyInfo.js
@@ -1,5 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency){
-    useEffect(()=>{}  , []) ;   
+    let [data , setData] = useState({}) ; 
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        .then((response)=> response.json())
+        .then((response)=> setData(response[currency]))
+
+        console.log(data) ;
+        return data ;  
+    }  , [currency]) ;     
 }
+
+export default useCurrencyInfo
